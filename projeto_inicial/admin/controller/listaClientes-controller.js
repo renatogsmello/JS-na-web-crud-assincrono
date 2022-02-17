62d206d5ea58cd182b44104d22d9b4f1ce0aca46
renatogsmello <renato.mello@globalhitss.com.br>
2022-02-17 16:22:41 -0300

    commit inicial

---
+++ projeto_inicial/admin/controller/listaClientes-controller.js
@@ -0,0 +1,40 @@
+import { clienteService } from "../service/cliente-service.js"
+const criaLinha = (nome, email, id) => {
+	const linhaNovoCliente = document.createElement("tr")
+	const conteudo = `
+        <td class="td" data-td>${nome}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="tabela__botoes-controle">
+                <li><a href="../telas/edita_cliente.html?${id}" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+            </ul>
+        </td>
+    `
+
+	linhaNovoCliente.innerHTML = conteudo
+	linhaNovoCliente.dataset.id = id
+	return linhaNovoCliente
+}
+
+const tabela = document.querySelector("[data-tabela]")
+
+tabela.addEventListener("click", (evento) => {
+	const botaoDeleta =
+		evento.target.className === "botao-simples botao-simples--excluir"
+	if (botaoDeleta) {
+		const linhaCliente = evento.target.closest("[data-id]")
+		let id = linhaCliente.dataset.id
+		clienteService.removeCliente(id).then(() => {
+			linhaCliente.remove()
+		})
+	}
+})
+
+clienteService.listaClientes().then((data) => {
+	data.forEach((elemento) => {
+		tabela.appendChild(
+			criaLinha(elemento.nome, elemento.email, elemento.id)
+		)
+	})
+})
---
+++ projeto_inicial/admin/service/cliente-service.js
@@ -0,0 +1,32 @@
+const listaClientes = () => {
+	return fetch(`http://localhost:3000/profile`).then((resposta) => {
+		return resposta.json()
+	})
+}
+
+const criaCliente = (nome, email) => {
+	return fetch(`http://localhost:3000/profile`, {
+		method: "POST",
+		headers: {
+			"content-type": "application/json",
+		},
+		body: JSON.stringify({
+			nome: nome,
+			email: email,
+		}),
+	}).then((resposta) => {
+		resposta.body
+	})
+}
+
+const removeCliente = (id) => {
+	return fetch(`http://localhost:3000/profile/${id}`, {
+		method: "DELETE",
+	})
+}
+
+export const clienteService = {
+	listaClientes,
+	criaCliente,
+	removeCliente,
+}
